b3facec008378154a0100e1f0779cdf9f78f8e04
Michael Garcia <mikeinc30@gmail.com>
2026-03-14 21:23:05 -0400

    fixed some bugs + finished the function to recursivly search for the correct file

--- cpp-file-copy-utility/autosave.cpp
+++ cpp-file-copy-utility/autosave.cpp
@@ -6,8 +6,9 @@
 using std::cout, std::cin, std::endl;
 
 namespace autosave_tools {
-    void autosave(){ 
+    
         namespace fs = std::filesystem;
+
         std::string findFile(const std::string &filename){
             std::string docs = std::getenv("HOME") + std::string("/Documents"); // Assuming the file is in the Documents directory
             for (const auto &entry : fs::recursive_directory_iterator(docs)){
@@ -17,13 +18,20 @@ namespace autosave_tools {
             }
             return ""; // Return an empty string if the file is not found
         }
+    void autosave(){ 
+
         std::string line;
         std::string SourceName;
         std::string DestName;
         cout<<"Enter File to copy from:  " << endl; 
         std::getline(cin, SourceName);
 
-        std::ifstream source(SourceName);
+        std::string fullPath = findFile(SourceName);
+        if (fullPath.empty()){
+            std::cerr <<"File not found in Documents directory.\n"<<endl; 
+            return;
+        }
+        std::ifstream source(fullPath);
 
         if (!source){
             std::cerr <<"Could not find the file to read from\n"<<endl; 
